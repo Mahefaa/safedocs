c4d3890e2bda9abd1328d319c1eb78c9e090434f
Mahefaa <hei.mahefa@gmail.com>
2025-10-21 09:55:58 +0200

    chore: env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import {createClient} from '@supabase/supabase-js';
 
-const supabaseUrl = "";
-const supabaseAnonKey = "";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Missing Supabase environment variables');
 }
